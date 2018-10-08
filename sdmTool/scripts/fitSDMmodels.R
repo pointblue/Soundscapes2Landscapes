ca14c07ba135e3c9ea1569a159ec8af4fb56e8b2
lsalas <lsalas@PETLP3389.prbo.org>
2018-10-08 15:48:18 -0700

    New data, updated data, and code

--- sdmTool/scripts/fitSDMmodels.R
+++ sdmTool/scripts/fitSDMmodels.R
@@ -84,7 +84,7 @@ getVarMetaClass<-function(df){
 	return(df)
 }
 
-# HERE: Loop through resolutions and species, all for the 3yr...
+# HERE: Loop through resolutions and species, all for the 3yr... VECTORIZE!
 for(zz in resolution){
 	#need to store in a single data frame:
 	# species, resolution, model, top10 vars, and their value
@@ -222,6 +222,143 @@ for(zz in resolution){
 }
 
 
+
+
+for(zz in resolution){
+	#need to store in a single data frame:
+	# species, resolution, model, top10 vars, and their value
+	topvars<-data.frame()
+	
+	#get the base grid for this resolution
+	basegrid<-raster(paste0(pathToGit,"Coast_Distance/",zz,"/CoastDIstance_",zz,"_Clip.tif"))
+	basegrid[]<-NA
+	
+	# Load the deflated bird file and filter for the loop species
+	dtpth<-paste0(pathToGit,"birds/",zz)
+	load(file=paste0(dtpth,"/NOGEDI_deflated_",zz,".RData"))	
+	
+	for(spcd in species){
+		#select only the desired species from the data
+		omitspecies<-subset(species,species!=spcd)
+		omitnumdet<-paste0("NumDet",omitspecies)
+		
+		#get covars and the current species' data
+		spdata<-deflatedcovardf[,which(!names(deflatedcovardf) %in% c(omitspecies,omitnumdet))]
+		spdata<-as.data.frame(na.omit(spdata))
+		#create the prediction grid
+		predgriddf<-deflatedcovardf[,which(!names(deflatedcovardf) %in% c(omitspecies,omitnumdet) & !names(deflatedcovardf) %in% c("x","y") & 
+								!names(deflatedcovardf) %in% c(spcd,paste0("NumDet",spcd)))]
+		predgriddf<-na.omit(predgriddf)
+		#will need this too...
+		xydf<-deflatedcovardf[,c("x","y",paste0("gId",zz))];names(xydf)<-c("x","y","cellId")
+		
+		# Then fit the stack, predict, and save the wighted average of all models
+		spdata<-spdata[,which(!names(spdata) %in% c("x","y",paste0("gId",zz)))]
+		
+		#Need to vectorize this LEO!
+		if(sum(spdata[,spcd]==1)>0.05*nrow(spdata)){
+			names(spdata)<-gsub(spcd,"PresAbs",names(spdata))
+			spdata$PresAbs_f<-as.factor(as.character(spdata$PresAbs))
+			
+			## make train and test sets  STRATIFY!
+			spdata<-stratifySample(df=spdata,yvar="PresAbs_f",percTrain=percent.train)
+			trainset<-subset(spdata,inOut==1);testset<-subset(spdata,inOut==0)
+			
+			trainsize<-round(percent.train*nrow(spdata))	#setting train size to 80%
+			naivePrev<-sum(spdata$PresAbs)/nrow(spdata)
+			
+			## fitting  models
+			nc<-ncol(trainset)-4
+			fmlf<-paste("PresAbs_f~",paste(names(trainset[1:nc]),collapse="+"),sep="")
+			fmln<-paste("PresAbs~",paste(names(trainset[3:nc]),collapse="+"),sep="")
+			svmm<-fit(as.formula(fmlf), data=trainset, model="svm", cross=10, C=2)
+			rfom<-fit(as.formula(fmlf), data=trainset, model="randomForest",na.action=na.omit,importance=TRUE)
+			boom<-fit(as.formula(fmlf), data=trainset, model="boosting",na.action=na.omit)
+			xgbm<-try(fitXGB(trainset,testset,deflatedcovardf,predgriddf),silent=TRUE)
+			
+			## predicting to stack
+			preds<-data.frame(cellId=as.integer(predgriddf[,paste0("gId",zz)]))
+			prfom<-as.data.frame(predict(rfom,predgriddf))
+			preds$vrfom<-as.numeric(prfom[,2])
+			psvmm<-as.data.frame(predict(svmm,predgriddf))
+			preds$vsvmm<-as.numeric(psvmm[,2])
+			pboom<-as.data.frame(predict(boom,predgriddf))
+			preds$vboom<-as.numeric(pboom[,2])
+			if(!inherits(xgbm,"try-error")){preds$vxgbm<-xgbm$predgrid}
+			
+			## predict to test set and eval the rmse
+			test<-data.frame(observed=testset[,"PresAbs"])
+			trfom<-as.data.frame(predict(rfom,testset))
+			test$prfo<-as.numeric(trfom[,2])
+			tsvmm<-as.data.frame(predict(svmm,testset))
+			test$psvm<-as.numeric(tsvmm[,2])
+			tboom<-as.data.frame(predict(boom,testset))
+			test$pboo<-as.numeric(tboom[,2])
+			if(!inherits(xgbm,"try-error")){
+				test$xgbm<-xgbm$predtest
+			}
+			
+			## individual model support is then:
+			supp<-apply(test[,2:ncol(test)],2,FUN=function(x,obs)sqrt(sum((x-obs)^2)/NROW(x)),obs=test$observed)
+			mv<-ceiling(max(supp));supp<-mv-supp
+			
+			save(trainset,testset,test,supp,rfom,svmm,boom,xgbm, file=paste0(svpth,zz,"/",spcd,"_",zz,"NOGEDI_modelResults.RData"))
+			
+			## convert predicted values to logits...
+			#preds<-adply(.data=preds[,2:5],.margins=1,.fun=function(x)log(x)-log(1-x))	#Too slow!
+			preds<-data.table(preds)
+			preds[,lgvrfom:=log(vrfom)-log(1-vrfom),]
+			preds[,lgvsvmm:=log(vsvmm)-log(1-vsvmm),]
+			preds[,lgvboom:=log(vboom)-log(1-vboom),]
+			preds[,lgvxgbm:=log(vxgbm)-log(1-vxgbm),]
+			
+			## and weighted average is...
+			ssup<-sum(supp)
+			preds[,lgweighted:=apply(X=preds,MARGIN=1,FUN=function(x,supp,ssup)as.numeric(x[6:9])%*%supp/ssup,supp=supp,ssup=ssup),]
+			## convert it back to probabilities...
+			preds[,weighted:=exp(lgweighted)/(1+exp(lgweighted)),]
+			
+			## convert to raster and plot...
+			rastres<-basegrid
+			preds<-merge(preds,xydf,by="cellId",all.x=T)
+			preds$cid<-cellFromXY(basegrid,preds[,c("x","y")])
+			cid<-preds$cid;vals<-as.numeric(preds$weighted)
+			rastres[cid]<-vals
+			plot(rastres)
+			writeRaster(rastres,filename=paste0(svpth,zz,"/",spcd,"_",zz,"NOGEDI_probPresence.tif",sep=""),format="GTiff",overwrite=T)
+			print(paste("Done with",spcd))
+			## let's hurdle it by the naive prevalence...
+			preds[,presence:=ifelse(weighted<=naivePrev,0,1),]
+			trastres<-basegrid
+			vals<-as.numeric(preds$presence)
+			trastres[cid]<-vals
+			plot(trastres)
+			## write as geotiff
+			writeRaster(rastres,filename=paste0(svpth,zz,"/",spcd,"_",zz,"NOGEDI_hurdle.tif",sep=""),format="GTiff",overwrite=T)
+			
+			#compile variable importance-top 10
+			imptemp<-data.frame()
+			impsvm<-retrieveVarImp(mdl=svmm,trainset=trainset,type="SVM");imptemp<-rbind(imptemp,impsvm)
+			imprfo<-retrieveVarImp(mdl=rfom,trainset=trainset,type="RandomForests");imptemp<-rbind(imptemp,imprfo)
+			impboo<-retrieveVarImp(mdl=boom,trainset=trainset,type="AdaBoost");imptemp<-rbind(imptemp,impboo)
+			impxgb<-xgbm$varimp[,c("FeatureName","Gain")];names(impxgb)<-c("Variable","AbsImportance")
+			impxgb$Model<-"xgBoost";impxgb<-impxgb[1:10,]
+			impxgb$RelImportance<-lapply(impxgb$AbsImportance,FUN=function(x,sumI){absi<-x/sumI;return(absi)},sumI=sum(impxgb$AbsImportance))
+			imptemp<-rbind(imptemp,impxgb)
+			imptemp<-getVarMetaClass(df=imptemp)
+			imptemp$Species<-spcd;imptemp$Resolution<-zz
+			
+			topvars<-rbind(topvars,imptemp)
+			
+			print(paste("Done with",spcd,"at resolution",zz))
+			
+		}else{
+			print(paste("Skipping",spcd,"at resolution",zz,"because of <5% of sites have presence."))
+		}
+		
+	}
+	save(topvars,file=paste0(svpth,zz,"/NOGEDI_topVariables_",zz,".RData"))
+}
 
 
 
--- sdmTool/scripts/plotRMSE.R
+++ sdmTool/scripts/plotRMSE.R
@@ -14,6 +14,8 @@ pathToData<-"c:/temp/s2l/250M/"
 
 resfiles<-list.files(pathToData,pattern=".RData")
 resfiles<-subset(resfiles,!grepl("topVariables",resfiles))
+resfls<-subset(resfiles,!grepl("NOGEDI",resfiles))
+
 
 species<-data.frame(spcd=c("ACWO","CALT","WESJ","MODO","NOMO","OATI","AMGO","LEGO","SPTO","DEJU", "HOFI","BLPH","RWBL","SOSP", "WCSP"),
 		habitat=c("Oak","Oak","Oak","Oak","Oak","Oak","Grass","Grass","Conn","Conn","Urb","Rip","Rip","Rip","Scrub"),
@@ -22,7 +24,7 @@ species<-data.frame(spcd=c("ACWO","CALT","WESJ","MODO","NOMO","OATI","AMGO","LEG
 species<-subset(species, spcd %in% c("ACWO","WESJ","BLPH","RWBL","AMGO","LEGO","DEJU","SPTO"))
 
 rmsedf<-data.frame()
-for(ff in resfiles){
+for(ff in resfls){
 	load(paste0(pathToData,ff))
 	tdf<-data.frame(model=names(supp),support=as.numeric(supp),Species=substr(ff,1,4))
 	rmsedf<-rbind(rmsedf,tdf)
@@ -46,3 +48,31 @@ p<-ggplot(rmsedf,aes(x=Model,y=RMSE)) +
 jpeg(file="c:/users/lsalas/desktop/GEDI_RMSE_250M.jpeg",quality=100, width=400, height=200)
 print(p);dev.off()
 
+
+## No Gedi
+resfls<-subset(resfiles,grepl("NOGEDI",resfiles))
+
+rmsedf<-data.frame()
+for(ff in resfls){
+	load(paste0(pathToData,ff))
+	tdf<-data.frame(model=names(supp),support=as.numeric(supp),Species=substr(ff,1,4))
+	rmsedf<-rbind(rmsedf,tdf)
+}
+rmsedf$RMSE<-1-rmsedf$support
+
+rmsedf<-subset(rmsedf,Species %in% species$spcd)
+rmsedf<-merge(rmsedf,species,by.x="Species",by.y="spcd",all.x=T)
+rmsedf$Species<-reorder(rmsedf$Species,rmsedf$plotorder)
+rmsedf$Model<-ifelse(rmsedf$model=="pboo","AdaBoost",
+		ifelse(rmsedf$model=="prfo","RandomForests",
+				ifelse(rmsedf$model=="psvm","SupportVector","XGBoost")))
+meanRMSE<-mean(rmsedf$RMSE)
+
+p<-ggplot(rmsedf,aes(x=Model,y=RMSE)) + 
+		geom_hline(yintercept=meanRMSE,color="black", linetype="dashed") +
+		geom_point(aes(color=Species),size=2) +
+		scale_y_continuous(limits=c(0,0.5), breaks=seq(0,0.5,by=0.1)) +
+		labs(x="Model")
+
+jpeg(file="c:/users/lsalas/desktop/NO_GEDI_RMSE_250M.jpeg",quality=100, width=400, height=200)
+print(p);dev.off()
