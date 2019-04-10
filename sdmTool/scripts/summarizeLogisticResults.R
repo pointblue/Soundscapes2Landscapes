4b3478e6143d0416290d18de1fa79e6cbef82d06
lsalas <lsalas@PETLP3389.prbo.org>
2019-04-10 12:58:30 -0700

    Added code to process the bootstraps for the logistic regressions, and extended code to process the GEDI, BCM and NDVI variables alone. Also centered VIF standards to 2yrs + 500M

--- sdmTool/scripts/summarizeLogisticResults.R
+++ sdmTool/scripts/summarizeLogisticResults.R
@@ -12,32 +12,59 @@ species<-c("WESJ", "HOFI", "CALT", "BLPH", "DEJU", "WCSP", "OATI", "BRBL", "RWBL
 		"WBNU", "PISI", "WEME", "WREN", "PUFI", "SAVS", "BRCR", "WIWA", "BHGR")
 resolution<-c("250M","500M","1000M") #
 gediyrs<-c("1yr","2yr","3yr")
-svpath<-"c:/s2ltemp/sdmtool/results/"
+datapth<-"//prbo.org/Data/Home/Petaluma/lsalas/Documents/lsalas/Mateo/GEDIpaper/LogisticRegResults/"
 
-sumdf<-data.frame()
+
+sumdfa<-data.frame()
+sumdfb<-data.frame()
 for(zz in resolution){
 	for(gg in gediyrs){
 		for(ss in species){
-			filen<-paste0(svpath,zz,"/",ss,"_",zz,"_",gg,"_logisticModelResults.RData")
-			if(file.exists(filen)){
-				load(filen)
-				resdf$species<-ss;resdf$resolution<-zz;resdf$gediyr<-gg
-				resdf$signif<-ifelse(resdf$LRTpval<0.05,1,0)
-				sumdf<-rbind(sumdf,resdf)
+			filena<-paste0(datapth,zz,"/",ss,"_",zz,"_",gg,"__AsIs_logisticModelResults.RData")
+			if(file.exists(filena)){
+				load(filena)
+				rdf<-res$resdf
+				rdf$species<-ss;rdf$resolution<-zz;rdf$gediyr<-gg
+				rdf$signif<-ifelse(rdf$LRTpval<0.05,1,0)
+				sumdfa<-rbind(sumdfa,rdf)
+				rm(list=c("res","rdf"));gc()
 			}
-			
+			filenb<-paste0(datapth,zz,"/",ss,"_",zz,"_",gg,"__balanced_logisticModelResults.RData")
+			if(file.exists(filenb)){
+				load(filenb)
+				#there are 500 bootstraps, so we aregoing to profile the parameters from these 
+				brdf<-ldply(.data=1:500,.fun=function(x,res){
+							rdf<-res[[x]]$resdf
+							rdf$species<-ss;rdf$resolution<-zz;rdf$gediyr<-gg
+							rdf$signif<-ifelse(rdf$LRTpval<0.05,1,0)
+							rdf$bootNum<-x
+							return(rdf)
+						},res=res)
+				sumdfb<-rbind(sumdfb,brdf)
+				rm(list=c("res","brdf"));gc()
+			}
 		}
 	}
 }
 
-p1<-ggplot(data=subset(sumdf,Model=="WithGEDIoptimized" & gediyr=="3yr" & resolution=="250M" & LRTpval<0.5),aes(x=species,y=LRTpval)) + geom_point(aes(color=signif)) +
+p1a<-ggplot(data=subset(sumdfa,Model=="WithGEDIoptimized" & gediyr=="3yr" & resolution=="250M" & LRTpval<0.5),aes(x=species,y=LRTpval)) + geom_point(aes(color=signif)) +
 		coord_flip() + theme(legend.position="none")
 
-p2<-ggplot(data=subset(sumdf,Model=="WithGEDIoptimized" & gediyr=="3yr" & LRTpval<0.5),aes(x=species,y=LRTpval)) + geom_hline(yintercept=0.05,color="black") +
+p2a<-ggplot(data=subset(sumdfa,Model=="WithGEDIoptimized" & gediyr=="3yr" & LRTpval<0.5),aes(x=species,y=LRTpval)) + geom_hline(yintercept=0.05,color="black") +
 		geom_point(aes(color=resolution),position=position_dodge(width = 0.5)) +
 		coord_flip() 
 
-p3<-ggplot(data=subset(sumdf,Model=="WithGEDIoptimized" & resolution=="250M" & LRTpval<0.5),aes(x=species,y=LRTpval)) + geom_hline(yintercept=0.05,color="black") +
+p3a<-ggplot(data=subset(sumdfa,Model=="WithGEDIoptimized" & resolution=="250M" & LRTpval<0.5),aes(x=species,y=LRTpval)) + geom_hline(yintercept=0.05,color="black") +
 		geom_point(aes(color=gediyr),position=position_dodge(width = 0.5)) +
 		coord_flip() 
 
+p1b<-ggplot(data=subset(sumdfb,Model=="WithGEDIoptimized" & gediyr=="3yr" & resolution=="250M" & LRTpval<0.5),aes(x=species,y=LRTpval)) + geom_point(aes(color=signif)) +
+		coord_flip() + theme(legend.position="none")
+
+p2b<-ggplot(data=subset(sumdfb,Model=="WithGEDIoptimized" & gediyr=="3yr" & LRTpval<0.5),aes(x=species,y=LRTpval)) + geom_hline(yintercept=0.05,color="black") +
+		geom_point(aes(color=resolution),position=position_dodge(width = 0.5)) +
+		coord_flip() 
+
+p3b<-ggplot(data=subset(sumdfb,Model=="WithGEDIoptimized" & resolution=="250M" & LRTpval<0.5),aes(x=species,y=LRTpval)) + geom_hline(yintercept=0.05,color="black") +
+		geom_point(aes(color=gediyr),position=position_dodge(width = 0.5)) +
+		coord_flip() 
