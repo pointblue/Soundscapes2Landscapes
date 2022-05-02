95197fd5df532ba0bef1c7c42640eca73553c819
Colin <44040608+CQuinn8@users.noreply.github.com>
2022-05-02 16:51:52 -0700

    Update cnn_pretrain_gpus.sh

--- CNN_Bird_Species/CNN_pretraining/cnn_pretrain_gpus.sh
+++ CNN_Bird_Species/CNN_pretraining/cnn_pretrain_gpus.sh
@@ -4,7 +4,7 @@
 # ----- ABOUT -----
 # By Colin Quinn, NAU
 # cq73@gmail.com
-# Created: 2020/Nov/9
+# Created: 2020-Nov-9
 #
 # Purpose: pretrain IMAGENET weighted CNN architectures (resnet50, resnet101, mobilenet) with XC data
 # you need to always have a cpu per gpu
@@ -12,14 +12,14 @@
 
 # ----- INPUTS -----
 # - SLURM -
-#SBATCH --job-name=RES1
-#SBATCH --output=/scratch/cq73/projects/S2L/cnn_pretrain/results/cnn_pretrain_%A.log
+#SBATCH --job-name=RES1                                     # CNN name for Job ID                                        
+#SBATCH --output=cnn_pretrain_%A.log                            
 #SBATCH --partition=core
-#SBATCH --time=24:00:00
-#SBATCH --mem=350GB
+#SBATCH --time=24:00:00                                     # take approx 18-24 hours 
+#SBATCH --mem=350GB                                         # large amount of memory to load XC dataset in as coded in training 
 #SBATCH --cpus-per-task=10
 #SBATCH --chdir=/scratch/cq73/projects/S2L/cnn_pretrain/
-#SBATCH --gres=gpu:tesla:1
+#SBATCH --gres=gpu:tesla:1                                  # request one Tesla V100-SXM2-16GB
 
 
 # ----- PROCESSING -----
@@ -32,7 +32,7 @@ echo "SLURM_JOBID: "$SLURM_JOBID
 echo
 
 # - MODULES -
-module load anaconda3/2020.07 #.02 for older envs
+module load anaconda3/2020.07 
 module load cuda/10.2
 
 # - PYTHON ENV -
@@ -42,10 +42,9 @@ echo
 
 # - Scripts -
 echo "---------Entering Python Script---------"
-# One script for each CNN architecture
-#python3.7 /scratch/cq73/projects/S2L/cnn_pretrain/code/cnn_train/mobnet_pretrain.py
-#python3.7 /scratch/cq73/projects/S2L/cnn_pretrain/code/cnn_train/resnet50v2_pretrain.py
-python3.7 /scratch/cq73/projects/S2L/cnn_pretrain/code/cnn_train/resnet101v2_pretrain.py
+# One script for each CNN architecture - here example for mobilenet
+python3.7 /scratch/cq73/projects/S2L/cnn_pretrain/code/cnn_train/mobnet_pretrain.py
+
 
 # - ENDING -
 echo "Ended at:"
