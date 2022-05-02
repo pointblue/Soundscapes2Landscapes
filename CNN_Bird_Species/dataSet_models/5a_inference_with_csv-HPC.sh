400967ea3f02ad24e319c6c44e884f7df345cf4f
Colin <44040608+CQuinn8@users.noreply.github.com>
2022-05-02 16:17:42 -0700

    Update 5a_inference_with_csv-HPC.sh

--- CNN_Bird_Species/dataSet_models/5a_inference_with_csv-HPC.sh
+++ CNN_Bird_Species/dataSet_models/5a_inference_with_csv-HPC.sh
@@ -7,6 +7,7 @@
 # cq73@gmail.com
 # Created: 8/18/2020
 # PURPOSE: create slurm job (part1) based on number of files to compute bird species probabilities (part2)
+# NOTE: user inputs include lines 31 - 41
 
 # Part 1: Create SLURM array based on nuber of recordings to derive probs
 # - SLURM -
@@ -18,23 +19,24 @@ echo "Entering first slurm script"
 # --- SLURM Settings ---
 arrayLim=2000        # how many array jobs to have at once
 files_per_task=100   # how large each job should be, number of files
-timeReq="01:00:00" 
+timeReq="01:00:00"   # HH:mm:ss
 memoryReq="2GB"      # low memory usage regardless of files_per_task or time
 
 # where output, first .sh script will be written to
 script_dir='/projects/tropics/users/cquinn/s2l/code/bird_cnn_inference/'
 
+
+# --- USER INPUTS ---
 # --- MODEL BUILDS ---
-# CNN trained on XC_warbler: comment out desired model for each run
-#cnn_path='/scratch/cq73/projects/S2L/cnn_inference/data/cols_54cls_mobnet_full_finetune/my_model/'
-#cnn_path='/scratch/cq73/projects/S2L/cnn_inference/data/cols_54cls_Resnet101/my_model/'
-cnn_path='/scratch/cq73/projects/S2L/cnn_inference/data/cols_54cls_Resnet50/my_model/'
+# CNN trained on XC_warbler and S2L bird data. Comment out desired model for each run.
+#cnn_path='/scratch/cq73/projects/S2L/cnn_inference/data/cols_54cls_mobnet_full_finetune/my_model/' # MobileNet v2
+#cnn_path='/scratch/cq73/projects/S2L/cnn_inference/data/cols_54cls_Resnet101/my_model/'            # ResNet v2 101
+cnn_path='/scratch/cq73/projects/S2L/cnn_inference/data/cols_54cls_Resnet50/my_model/'              # ResNet v2 50
 
 # --- DATA & RESULT DIR Settings ---
-# results dir should live in /projects/ and have a sigmoid folder within the target
-results_dir='/projects/tropics/users/cquinn/s2l/S2L_RA_work/cnn_inference/2021/cols_54cls_Resnet50/' # Model output
-tag="2021"                                                                                           # Audio year pre2020 or 2021 - not vital jsut used for logs
-todo_csv='/projects/tropics/users/cquinn/s2l/S2L_RA_work/cnn_inference/2021/toDo_RN50_toDo.csv'      # CSV built from inference_csv_builder.R 
+# results dir should have a sigmoid folder within the target already built
+results_dir='/projects/tropics/users/cquinn/s2l/S2L_RA_work/cnn_inference/2021/cols_54cls_Resnet50/' # Model output folder matches model name
+todo_csv='/projects/tropics/users/cquinn/s2l/S2L_RA_work/cnn_inference/2021/toDo_RN50_toDo.csv'      # CSV listing files still requiring inference (difference completed vs all files)
 logs_path='/projects/tropics/users/cquinn/s2l/S2L_RA_work/cnn_inference/2021/logs/'                  # job logs
 num_files=749296                                                                                     # number of remaining files for inference (rows in csv)
 
@@ -50,14 +52,14 @@ echo "Number of tasks ="$ntask
 njobs=$(echo "($ntask+0.999)/1" | bc) # ceiling round number of tasks
 echo "Number of jobs ="$njobs
 
-slurmLogName=$script_dir'logs/'$tag'_%a.out' # Specify the basename for the output log file
+slurmLogName=$script_dir'logs/%a.out' # Specify the basename for the output log file
 
 
 # PART 2: create SLURM array based on ntasks for inference
 # --- SLURM ARRAY ---
 # Create the slurm array job script in the slurm folder
 cd $script_dir
-scriptName='sbatch/sbatch_melspec_gen_'$date_time'.sh'
+scriptName='sbatch/sbatch_inference_'$date_time'.sh'
 
 cat > $scriptName <<EOT
 #!/bin/bash
@@ -90,7 +92,7 @@ conda activate tfgpu           # activate cnn env
 echo
 echo "---------Entering script---------"
 # --- SCRIPT ---
-python3.7 /projects/tropics/users/cquinn/s2l/code/bird_cnn_inference/1b_inference_HPC_model_with_csv.py \$SLURM_ARRAY_TASK_ID $files_per_task $todo_csv $results_dir $cnn_path $logs_path
+python3.7 /projects/tropics/users/cquinn/s2l/code/bird_cnn_inference/5b_inference_HPC_model_with_csv.py \$SLURM_ARRAY_TASK_ID $files_per_task $todo_csv $results_dir $cnn_path $logs_path
 
 # - ENDING -
 echo "Ended at:"
